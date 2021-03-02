a7117c00777d509ccfcbf655ddf8ac98b0c24f89
Donghyeon Park <donpark@c6r10s8.42seoul.kr>
2021-03-03 04:12:23 +0900

    del

--- gnltesterclone.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/charMstr/GNL_lover.git
-git clone https://github.com/DontBreakAlex/gnlkiller.git
-git clone https://github.com/Hellio404/Get_Next_Line_Tester.git
-git clone https://github.com/mrjvs/42cursus_gnl_tests.git
-git clone https://github.com/Mazoise/42TESTERS-GNL.git
