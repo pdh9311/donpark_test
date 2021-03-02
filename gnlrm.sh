1d6187d6af255f3283dd720c3d79d2c3c5d0ea18
Donghyeon Park <donpark@c6r10s8.42seoul.kr>
2021-03-03 04:13:15 +0900

    modify

--- gnlrm.sh
+++ gnlrm.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 rm -rf ./get_next_line.c ./get_next_line_utils.c ./get_next_line.h
+rm -rf ./42cursus_gnl_tests/get_next_line
 rm -rf ./gnlkiller/get_next_line.c ./gnlkiller/get_next_line_utils.c ./gnlkiller/get_next_line.h
 rm -rf ./GNL_lover/copy_in_here_GNL_files/get_next_line.c ./GNL_lover/copy_in_here_GNL_files/get_next_line_utils.c ./GNL_lover/copy_in_here_GNL_files/get_next_line.h 
--- gnltest.sh
+++ gnltest.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
-#42TESTER-GNL, 42curus_gnl_tests, Get_Next_Line_Tester
+
+#42TESTER-GNL, Get_Next_Line_Tester
 cp get_next_line/get_next_line.c ./ 
 cp get_next_line/get_next_line_utils.c ./
 cp get_next_line/get_next_line.h ./
+
+#42cursus_gnl_tests
+chmod 777 ./42cursus_gnl_tests/compile.sh
+chmod 777 ./42cursus_gnl_tests/compile_and_run.sh
+mkdir ./42cursus_gnl_tests/get_next_line
+cp get_next_line/get_next_line.c ./42cursus_gnl_tests/get_next_line/ 
+cp get_next_line/get_next_line_utils.c ./42cursus_gnl_tests/get_next_line/ 
+cp get_next_line/get_next_line.h ./42cursus_gnl_tests/get_next_line/ 
+
 #GNL_lover
 cp get_next_line/get_next_line.c GNL_lover/copy_in_here_GNL_files/
 cp get_next_line/get_next_line_utils.c GNL_lover/copy_in_here_GNL_files/
@@ -11,4 +21,3 @@ cp get_next_line/get_next_line.h GNL_lover/copy_in_here_GNL_files/
 cp get_next_line/get_next_line.c gnlkiller/
 cp get_next_line/get_next_line_utils.c gnlkiller/
 cp get_next_line/get_next_line.h gnlkiller/
-
