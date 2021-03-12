8b2358ff92fa5803e6df9f9406160e53ce5620f7
pdh9311 <padohy@gmail.com>
2021-03-12 18:07:57 +0900

    20210312 18:07

---
+++ get_next_line/get_next_line_bonus.c
@@ -0,0 +1,89 @@
+#include "get_next_line_bonus.h"
+
+int	is_newline(char *backup)
+{
+	int	i;
+
+	i = 0;
+	while (backup[i])
+	{
+		if(backup[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	split_line(char **backup, char **line, int cut_idx, char *buf)
+{
+	char	*temp;
+	int		len;
+
+	if(buf)
+		free(buf);
+	(*backup)[cut_idx] = '\0';
+	*line = ft_strdup(*backup);
+	len = ft_strlen(*backup + cut_idx + 1);
+	if (len == 0)
+	{
+		free(*backup);
+		*backup = 0;
+		return (1);
+	}
+	temp = ft_strdup(*backup + cut_idx + 1);
+	free(*backup);
+	*backup = temp;
+	return (1);
+}
+
+int	remains_data(char **backup, char **line, int read_size, char *buf)
+{
+	int	cut_idx;
+
+	if (buf)
+	{
+		free(buf);
+		buf = 0;
+	}
+	if (read_size < 0)
+		return (-1);
+	if (*backup && (cut_idx = is_newline(*backup)) != -1)
+		return (split_line(backup, line, cut_idx, buf));
+	else if (*backup)
+	{
+		*line = *backup;
+		*backup = 0;
+		return (0);
+	}
+	*line = ft_strdup("");
+	return (0);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	int			read_size;
+	char		*buf;
+	static char	*backup[OPEN_MAX];
+	int			cut_idx;
+	char		*tmp;
+
+	if (BUFFER_SIZE <= 0 || fd < 0 || fd > OPEN_MAX || !line)
+		return (-1);
+	if (!(buf = (char *)malloc(BUFFER_SIZE + 1)))
+		return (-1);
+	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+
+		buf[read_size] = '\0';
+		tmp = ft_strjoin(backup[fd], buf);
+		if(backup[fd])
+		{
+			free(backup[fd]);
+			backup[fd] = 0;
+		}
+		backup[fd] = tmp;
+		if ((cut_idx = is_newline(backup[fd])) != -1)
+			return (split_line(&backup[fd], line, cut_idx, buf));
+	}
+	return (remains_data(&backup[fd], line, read_size, buf));
+}
--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,5 +1,5 @@
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/stat.h>
@@ -12,7 +12,7 @@
 # endif
 
 # ifndef OPEN_MAX
-#  define OPEN_MAX 65536	// sysconf(_SC_OPEN_MAX)
+#  define OPEN_MAX 65535	// sysconf(_SC_OPEN_MAX)
 # endif
 
 int		is_newline(char *backup);
@@ -20,6 +20,6 @@ int		split_line(char **backup, char **line, int cut_idx, char *buf);
 int		remains_data(char **backup, char **line, int read_size, char *buf);
 int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
 #endif
