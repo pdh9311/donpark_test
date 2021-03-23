81f89b9b6b94c6b57e4d178f6e7661f4edf6d3c8
pdh9311 <padohy@gmail.com>
2021-03-23 18:39:00 +0900

    20210323 18:38

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -13,9 +13,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
 # include <unistd.h>
 # include <limits.h>
 
--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -13,9 +13,6 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 # include <stdlib.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
 # include <unistd.h>
 # include <limits.h>
 
