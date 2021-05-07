5233830b396a96ea304170d13fbde06765522b93
donpark <pdh6381@naver.com>
2021-05-07 18:33:35 +0900

    20210507 18:34

---
+++ Exam_Rank_02/gnl/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donpark <donpark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/22 03:27:48 by donpark           #+#    #+#             */
+/*   Updated: 2021/05/07 17:29:28 by donpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+int		ft_strlen(char *s);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+
+int		is_newline(char *backup);
+int		split_line(char **backup, char **line, int cut_idx, char *buf);
+int		remains_data(char **backup, char **line, int read_size, char *buf);
+int		get_next_line(char **line);
+#endif
