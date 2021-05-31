bd7a881137ebdaaa71292302c2bb787a7d0afc7a
pdh9311 <padohy@gmail.com>
2021-05-31 17:43:32 +0900

    print del

--- PRINTF/tester1.sh
+++
@@ -1,9 +0,0 @@
-./printfdel.sh
-cd ft_printf
-make
-make clean
-# PFT					[./enable-test 42(84), disable-test bonus , ./reset-to-default-enabled-tests , ./test]
-git clone https://github.com/gavinfielder/pft.git pft && echo "pft/" >> .gitignore
-cd pft
-./disable-test bonus
-./test
